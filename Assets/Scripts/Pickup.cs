66fc2699d6e084bd0b99ce0e19d3d117dd5574de
Daniel Hayes <dannyhayes57@hotmail.co.uk>
2016-08-21 15:13:18 +0100

    3rd Commit
    Camera follows player, Basic object pickups implemented.

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour 
+{
+	private Player_Controller player_controller;
+	private float speed, maxY, minY;
+	private Vector3 velocity;
+
+	// Rotation variables
+	private Quaternion rotation;
+	private Vector3 radius, CenterPoint;
+	private float currentRotation;
+
+	// Use this for initialization
+	void Start () 
+	{
+		speed = 0.02f;
+		minY = transform.position.y - 0.5f;
+		maxY = transform.position.y + 0.5f;
+		velocity = new Vector3(0, speed, 0);
+
+		CenterPoint = new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z);
+		currentRotation = 0.0f;
+		radius = new Vector3(5, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//transform.RotateAround(CenterPoint, new Vector3(0, 1, 0), 1.0f);
+
+		transform.position += velocity;
+
+		// create floating motion
+		if (velocity.y > 0)
+		{
+			if (transform.position.y >= maxY)
+			{
+				velocity = new Vector3(0, -speed, 0);
+			}
+		}
+		else
+		{
+			if (transform.position.y <= minY)
+			{
+				velocity = new Vector3(0, speed, 0);
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.transform.tag == "Player")
+		{
+			player_controller = collision.transform.GetComponentInChildren<Player_Controller>();
+			player_controller.Pickup();
+
+			Destroy(this.gameObject);
+		}
+	}
+}
--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -1,15 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player_Controller : MonoBehaviour {
+public class Player_Controller : MonoBehaviour 
+{
+	// Public members
+	public Material Blue, Red;
 
+	// Private Members
+	private enum CharacterStatus{ Default, Wizard, Astronaut, Pirate };
+	CharacterStatus CurrentStatus;
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		CurrentStatus = CharacterStatus.Default;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		switch(CurrentStatus)
+		{
+		case CharacterStatus.Default:
+			DefaultUpdate();
+			break;
+		case CharacterStatus.Wizard:
+			WizardUpdate();
+			break;
+		case CharacterStatus.Astronaut:
+			AstronautUpdate();
+			break;
+		case CharacterStatus.Pirate:
+			PirateUpdate();
+			break;
+		}
+	}
+
+	private void DefaultUpdate()
+	{
+
+	}
+
+	private void WizardUpdate()
+	{
+
+	}
+
+	private void AstronautUpdate()
+	{
+
+	}
+
+	private void PirateUpdate()
+	{
+
+	}
+
+	public void Pickup()
+	{
+		CurrentStatus = CharacterStatus.Wizard;
+
+		//
 	}
 }
